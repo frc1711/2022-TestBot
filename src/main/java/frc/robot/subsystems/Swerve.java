46f635fc4f0508e072965042b1be02645d6279e2
Gabriel Seaver <gabrielseaver9678@gmail.com>
2022-01-19 14:56:41 -0500

    stylistic changes and indentation

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -7,62 +7,63 @@ import frc.team1711.swerve.subsystems.SwerveDrive;
 import frc.team1711.swerve.util.Angles;
 
 public class Swerve extends AutoSwerveDrive {
-
-    // TODO: Get wheelbase ratio
-    private static final double widthToHeightWheelbaseRatio = 1;
-
-    private static final int
-        frontLeftSteerID = 6,
-        frontRightSteerID = 4,
-        rearLeftSteerID = 8,
-        rearRightSteerID = 2,
-
-        frontLeftDriveID = 5,
-        frontRightDriveID = 3,
-        rearLeftDriveID = 7,
-        rearRightDriveID = 1,
-
-        frontLeftSteerEncoderID = 0,
-        frontRightSteerEncoderID = 0,
-        rearLeftSteerEncoderID = 0,
-        rearRightSteerEncoderID = 0;
-        
-
-    private static final double
-        driveRelativeSpeed = SwerveDrive.driveRelativeSpeedDefault,
-        steerRelativeSpeed = SwerveDrive.steerRelativeSpeedDefault;
-
-    private final AHRS gyro;
-    
-    public Swerve () {
-        super(
-            new SwerveModule(frontLeftSteerID, frontLeftDriveID, frontLeftSteerEncoderID), // Front left module
-            new SwerveModule(frontRightSteerID, frontRightDriveID, frontRightSteerEncoderID), // Front right module
-            new SwerveModule(rearLeftSteerID, rearLeftDriveID, rearLeftSteerEncoderID), // Rear left module
-            new SwerveModule(rearRightSteerID, rearRightDriveID, rearRightSteerEncoderID), // Rear right module
-            widthToHeightWheelbaseRatio);
-        setDriveRelativeSpeed(driveRelativeSpeed);
-        setSteerRelativeSpeed(steerRelativeSpeed);
-
-        gyro = new AHRS();
-    }
-
-    public void configDirectionEncoders () {
-        ((SwerveModule)flWheel).configDirectionEncoder();
-        ((SwerveModule)frWheel).configDirectionEncoder();
-        ((SwerveModule)rlWheel).configDirectionEncoder();
-        ((SwerveModule)rrWheel).configDirectionEncoder();
-    }
-
-    @Override
-    public double getGyroAngle() {
-        return Angles.wrapDegrees(gyro.getAngle());
-    }
-
-    @Override
-    public void resetGyro() {
-        gyro.calibrate();
-        gyro.reset();
-    }
-
+	
+	// TODO: Get wheelbase ratio
+	private static final double widthToHeightWheelbaseRatio = 1;
+	
+	private static final int
+		frontLeftSteerID = 6,
+		frontRightSteerID = 4,
+		rearLeftSteerID = 8,
+		rearRightSteerID = 2,
+		
+		frontLeftDriveID = 5,
+		frontRightDriveID = 3,
+		rearLeftDriveID = 7,
+		rearRightDriveID = 1,
+		
+		// TODO: Get CANCoder IDs
+		frontLeftSteerEncoderID = 0,
+		frontRightSteerEncoderID = 0,
+		rearLeftSteerEncoderID = 0,
+		rearRightSteerEncoderID = 0;
+	
+	
+	private static final double
+		driveRelativeSpeed = SwerveDrive.driveRelativeSpeedDefault,
+		steerRelativeSpeed = SwerveDrive.steerRelativeSpeedDefault;
+	
+	private final AHRS gyro;
+	
+	public Swerve () {
+		super(
+			new SwerveModule(frontLeftSteerID, frontLeftDriveID, frontLeftSteerEncoderID), // Front left module
+			new SwerveModule(frontRightSteerID, frontRightDriveID, frontRightSteerEncoderID), // Front right module
+			new SwerveModule(rearLeftSteerID, rearLeftDriveID, rearLeftSteerEncoderID), // Rear left module
+			new SwerveModule(rearRightSteerID, rearRightDriveID, rearRightSteerEncoderID), // Rear right module
+			widthToHeightWheelbaseRatio);
+		setDriveRelativeSpeed(driveRelativeSpeed);
+		setSteerRelativeSpeed(steerRelativeSpeed);
+		
+		gyro = new AHRS();
+	}
+	
+	public void configDirectionEncoders () {
+		((SwerveModule)flWheel).configDirectionEncoder();
+		((SwerveModule)frWheel).configDirectionEncoder();
+		((SwerveModule)rlWheel).configDirectionEncoder();
+		((SwerveModule)rrWheel).configDirectionEncoder();
+	}
+	
+	@Override
+	public double getGyroAngle () {
+		return Angles.wrapDegrees(gyro.getAngle());
+	}
+	
+	@Override
+	public void resetGyro () {
+		gyro.calibrate();
+		gyro.reset();
+	}
+	
 }
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -14,107 +14,107 @@ import frc.team1711.swerve.subsystems.AutoSwerveWheel;
 import frc.team1711.swerve.util.Angles;
 
 public class SwerveModule extends AutoSwerveWheel {
-
-    // The reciprocal of this value is the unit in revolutions
-    // with which the direction absolute offset is stored
-    private static final int directionOffsetPrecision = 1000;
-
-    private static final double
-        steerPIDkp = 0.1,
-        steerPIDki = 0,
-        steerPIDkd = 0;
-
-    private final CANCoder steerEncoder;
-    private final PIDController steerPID;
-    private final double directionAbsoluteOffset;
-    private final CANSparkMax driveController, steerController;
-
-    public SwerveModule (int steerControllerID, int driveControllerID, int steerEncoderID) {
-        driveController = new CANSparkMax(steerControllerID, CANSparkMaxLowLevel.MotorType.kBrushless);
-        steerController = new CANSparkMax(driveControllerID, CANSparkMaxLowLevel.MotorType.kBrushless);
-
-        driveController.setIdleMode(IdleMode.kBrake);
-        steerController.setIdleMode(IdleMode.kBrake);
-
-        directionAbsoluteOffset = getDirectionAbsoluteOffset();
-        steerEncoder = new CANCoder(steerEncoderID);
-
-        steerPID = new PIDController(steerPIDkp, steerPIDki, steerPIDkd);
-    }
-
-    @Override
-    protected double getPositionDifference () {
-        return 0; // No autonomous capability
-    }
-
-    @Override
-    protected void resetDriveEncoder () { } // No autonomous capability
-
-    // Direction methods
-    @Override
-    protected double getDirection () {
-        return Angles.wrapDegrees(getRawDirection());
-    }
-
-    private double getRawDirection () {
-        return steerEncoder.getAbsolutePosition() - directionAbsoluteOffset;
-    }
-
-    // Absolute encoder direction offset
-    private double getDirectionAbsoluteOffset () {
-        // Retrieve direction from absolute encoder's arbitrary 0 value
-        return (double) steerEncoder.configGetCustomParam(0, directionOffsetPrecision) / directionOffsetPrecision;
-    }
-
-    private int createNewDirectionAbsoluteOffset () {
-        // Creates a new value for the encoder's direction absolute offset
-        // based on the assumption that the wheel is currently facing directly forward
-        return (int)Math.round(steerEncoder.getAbsolutePosition() * directionOffsetPrecision);
-    }
-
-    public void configDirectionEncoder () {
-        // Create the cancoder configuration
-        CANCoderConfiguration config = new CANCoderConfiguration();
-
-        // Use absolute position
-        config.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
-
-        // Output value from 0 to 360
-        config.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
-
-        // Clockwise is positive displacement
-        config.sensorDirection = true;
-
-        // Sets the direction absolute offset
-        config.customParam0 = createNewDirectionAbsoluteOffset();
-
-        // Flashes the configuration
-        steerEncoder.configAllSettings(config, 100);
-    }
-
-    // Controlling steering
-    @Override
-    protected void setDirection (double targetDirection) {
-        // Gets desired change in direction by wrapping the difference
-        // between where we want to be and where we with zero as the center
-        double directionChange = Angles.wrapDegreesZeroCenter(targetDirection - getDirection());
-
-        // PID loop between 0 (representing current value) and the number
-        // of revolutions we want to change the direction by
-        double steerSpeed = steerPID.calculate(0, directionChange / 360);
-
-        // Sets steering controller
-        steerController.set(steerSpeed);
-    }
-
-    @Override
-    protected void stopSteering () {
-        steerController.set(0);
-    }
-
-    @Override
-    protected void setDriveSpeed (double speed) {
-        driveController.set(speed);
-    }
-    
+	
+	// The reciprocal of this value is the unit in revolutions
+	// with which the direction absolute offset is stored
+	private static final int directionOffsetPrecision = 1000;
+		
+	private static final double
+		steerPIDkp = 0.1,
+		steerPIDki = 0,
+		steerPIDkd = 0;
+	
+	private final CANCoder steerEncoder;
+	private final PIDController steerPID;
+	private final double directionAbsoluteOffset;
+	private final CANSparkMax driveController, steerController;
+	
+	public SwerveModule (int steerControllerID, int driveControllerID, int steerEncoderID) {
+		driveController = new CANSparkMax(steerControllerID, CANSparkMaxLowLevel.MotorType.kBrushless);
+		steerController = new CANSparkMax(driveControllerID, CANSparkMaxLowLevel.MotorType.kBrushless);
+		
+		driveController.setIdleMode(IdleMode.kBrake);
+		steerController.setIdleMode(IdleMode.kBrake);
+		
+		directionAbsoluteOffset = getDirectionAbsoluteOffset();
+		steerEncoder = new CANCoder(steerEncoderID);
+		
+		steerPID = new PIDController(steerPIDkp, steerPIDki, steerPIDkd);
+	}
+	
+	@Override
+	protected double getPositionDifference () {
+		return 0; // No autonomous capability
+	}
+	
+	@Override
+	protected void resetDriveEncoder () { } // No autonomous capability
+	
+	// Direction methods
+	@Override
+	protected double getDirection () {
+		return Angles.wrapDegrees(getRawDirection());
+	}
+	
+	private double getRawDirection () {
+		return steerEncoder.getAbsolutePosition() - directionAbsoluteOffset;
+	}
+	
+	// Absolute encoder direction offset
+	private double getDirectionAbsoluteOffset () {
+		// Retrieve direction from absolute encoder's arbitrary 0 value
+		return (double) steerEncoder.configGetCustomParam(0, directionOffsetPrecision) / directionOffsetPrecision;
+	}
+	
+	private int createNewDirectionAbsoluteOffset () {
+		// Creates a new value for the encoder's direction absolute offset
+		// based on the assumption that the wheel is currently facing directly forward
+		return (int)Math.round(steerEncoder.getAbsolutePosition() * directionOffsetPrecision);
+	}
+	
+	public void configDirectionEncoder () {
+		// Create the cancoder configuration
+		CANCoderConfiguration config = new CANCoderConfiguration();
+		
+		// Use absolute position
+		config.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
+		
+		// Output value from 0 to 360
+		config.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+		
+		// Clockwise is positive displacement
+		config.sensorDirection = true;
+		
+		// Sets the direction absolute offset
+		config.customParam0 = createNewDirectionAbsoluteOffset();
+		
+		// Flashes the configuration
+		steerEncoder.configAllSettings(config, 100);
+	}
+	
+	// Controlling steering
+	@Override
+	protected void setDirection (double targetDirection) {
+		// Gets desired change in direction by wrapping the difference
+		// between where we want to be and where we with zero as the center
+		double directionChange = Angles.wrapDegreesZeroCenter(targetDirection - getDirection());
+		
+		// PID loop between 0 (representing current value) and the number
+		// of revolutions we want to change the direction by
+		double steerSpeed = steerPID.calculate(0, directionChange / 360);
+		
+		// Sets steering controller
+		steerController.set(steerSpeed);
+	}
+	
+	@Override
+	protected void stopSteering () {
+		steerController.set(0);
+	}
+	
+	@Override
+	protected void setDriveSpeed (double speed) {
+		driveController.set(speed);
+	}
+	
 }
