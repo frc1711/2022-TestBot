f731799c37e3562db7fc2f51d857ea7a9100a422
Gabriel Seaver <gabrielseaver9678@gmail.com>
2022-01-19 16:18:38 -0500

    Default command for swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,12 +9,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.SwerveTeleop;
 import frc.robot.subsystems.Swerve;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
 	
 	private final Swerve swerveDrive;
@@ -30,13 +24,10 @@ public class RobotContainer {
 			() -> driveController.getRawAxis(0),	// strafeX
 			() -> driveController.getRawAxis(0),	// strafeY
 			() -> driveController.getRawAxis(0));	// steering
+		
+		swerveDrive.setDefaultCommand(swerveTeleop);
 	}
 	
-	/**
-	 * Use this to pass the autonomous command to the main {@link Robot} class.
-	 *
-	 * @return the command to run in autonomous
-	 */
 	public Command getAutonomousCommand () {
 		return null;
 	}
